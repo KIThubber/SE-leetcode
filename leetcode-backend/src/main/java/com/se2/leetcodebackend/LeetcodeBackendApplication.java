298ad17d5db9bf1ed5d4258740fd880a2639adc1
jannis.fischer <Spiderman3%>
2022-05-04 10:14:37 +0200

    Created Spring project

---
+++ leetcode-backend/src/main/java/com/se2/leetcodebackend/LeetcodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.se2.leetcodebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeBackendApplication.class, args);
+    }
+
+}
