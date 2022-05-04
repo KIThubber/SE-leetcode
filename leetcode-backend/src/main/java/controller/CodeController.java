7cbcbd0d36f284829d845d7d822eb3836e5686e3
jannis.fischer <Spiderman3%>
2022-05-04 13:04:26 +0200

    Created Get request

--- leetcode-backend/src/main/java/controller/CodeController.java
+++ leetcode-backend/src/main/java/controller/CodeController.java
@@ -1,19 +1,34 @@
 package controller;
 
 
+import entity.CodeDAO;
 import entity.CodeDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import repository.CodeRepository;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/code/v1")
 public class CodeController {
+
+    @Autowired
+    CodeRepository codeRepository;
+
+    @GetMapping
+    public ResponseEntity<List<CodeDAO>> getCodeExamples() {
+        try {
+            return new ResponseEntity<List<CodeDAO>>(codeRepository.findAll(), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<List<CodeDAO>>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<List<CodeDAO>>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @PostMapping("/{userId}")
     public ResponseEntity<CodeDTO> createCodeExample(@PathVariable long userId, String code, String language) {
