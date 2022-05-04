f428fce36091aef05bd46068784c4fedd36a14d5
jannis.fischer <Spiderman3%>
2022-05-04 10:42:13 +0200

    Created Controller to manage Code requests

---
+++ leetcode-backend/src/main/java/controller/CodeController.java
@@ -0,0 +1,27 @@
+package controller;
+
+
+import entity.CodeDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/code/v1")
+public class CodeController {
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<CodeDTO> createCodeExample(@PathVariable long userId, String code, String language) {
+        try {
+            return null;
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<CodeDTO>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
